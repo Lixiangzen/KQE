f961ea6000f6dbb1bf0b79c6b6f9f6f6ec2e8f77
Lixiangzen <125168885+Lixiangzen@users.noreply.github.com>
2026-08-29 22:29:12 +0800

    fix: remove foojay toolchain plugin to fix CI build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,8 @@ pluginManagement {
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
 dependencyResolutionManagement {
